8710494bd77b9f307285bd22ef1c73ce4ea84c7f
Giorgia Fiscaletti <giorgia.fiscaletti@mia-platform.eu>
2022-08-04 16:42:42 +0200

    feat: test MoveToDisk

--- pkg/sync/sync_test.go
+++ pkg/sync/sync_test.go
@@ -15,18 +15,22 @@
 package sync
 
 import (
+	"path"
 	"testing"
 
 	"github.com/mia-platform/vab/internal/git"
+	"github.com/mia-platform/vab/internal/testutils"
 	"github.com/mia-platform/vab/pkg/apis/vab.mia-platform.eu/v1alpha1"
 	"github.com/mia-platform/vab/pkg/logger"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	testPackageName = "test-package-1.0.0"
-	mockFileName1   = "mockfile_1.yaml"
-	mockFileName2   = "mockfile_2.yaml"
+	testPackageName    = "test-package-1.0.0"
+	testModuleBasePath = "modules/test-module1"
+	testFileName1      = "test-flavour1/file1.yaml"
+	testFileName2      = "test-flavour1/file2.yaml"
+	testFileName3      = "test-flavour2/file1.yaml"
 )
 
 func TestClonePackage(t *testing.T) {
@@ -40,4 +44,26 @@ func TestClonePackage(t *testing.T) {
 		return
 	}
 	assert.NotNil(t, outputFiles, "The returned array of mocked file pointers is empty")
+}
+
+func TestMoveToDisk(t *testing.T) {
+	logger := logger.DisabledLogger{}
+	fakeWorktree := testutils.PrepareFakeWorktree(t)
+	input := []*git.File{
+		git.NewFile(path.Join(testModuleBasePath, testFileName1), testModuleBasePath, fakeWorktree),
+		git.NewFile(path.Join(testModuleBasePath, testFileName2), testModuleBasePath, fakeWorktree),
+		git.NewFile(path.Join(testModuleBasePath, testFileName3), testModuleBasePath, fakeWorktree),
+	}
+	testDirPath := t.TempDir()
+	err := MoveToDisk(logger, input, testPackageName, testDirPath)
+	if !assert.NoError(t, err) {
+		return
+	}
+	assert.FileExists(t, path.Join(testDirPath, testFileName1), "Mock file 1 does not exist on disk")
+	assert.FileExists(t, path.Join(testDirPath, testFileName2), "Mock file 2 does not exist on disk")
+	assert.FileExists(t, path.Join(testDirPath, testFileName3), "Mock file 3 does not exist on disk")
+}
+
+func TestSyncAddons(t *testing.T) {
+
 }
