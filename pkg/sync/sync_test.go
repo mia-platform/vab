fb3a164ed5ce40789edabf9e23ca75cbc2f55572
Giorgia Fiscaletti <giorgia.fiscaletti@mia-platform.eu>
2022-08-08 13:37:36 +0200

    feat: add UpdateBases tests

--- pkg/sync/sync_test.go
+++ pkg/sync/sync_test.go
@@ -15,18 +15,25 @@
 package sync
 
 import (
+	"os"
 	"path"
 	"testing"
 
 	"github.com/mia-platform/vab/internal/git"
 	"github.com/mia-platform/vab/internal/testutils"
+	"github.com/mia-platform/vab/internal/utils"
 	"github.com/mia-platform/vab/pkg/apis/vab.mia-platform.eu/v1alpha1"
 	"github.com/mia-platform/vab/pkg/logger"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	testPackageName = "test-module1/test-flavour1"
+	testPackageName       = "test-module1/test-flavour1"
+	expectedKustomization = `kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+resources:
+  - ../../../all-groups
+`
 )
 
 func TestClonePackage(t *testing.T) {
@@ -98,4 +105,49 @@ func TestSyncAddons(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
+}
+
+func TestUpdateBasesAllGroups(t *testing.T) {
+	testDirPath := t.TempDir()
+	targetPath := path.Join(testDirPath, utils.AllGroupsDirPath)
+	os.MkdirAll(targetPath, os.ModePerm)
+	modules := make(map[string]v1alpha1.Module)
+	modules["test-module1/test-flavour3"] = v1alpha1.Module{
+		Version: "1.0.0",
+		Weight:  4,
+	}
+	modules["test-module2/test-flavour2"] = v1alpha1.Module{
+		Version: "1.0.0",
+		Weight:  1,
+	}
+	modules["test-module2/test-flavour1"] = v1alpha1.Module{
+		Version: "1.0.0",
+		Weight:  3,
+		Disable: true,
+	}
+	addons := make(map[string]v1alpha1.AddOn)
+	addons["test-addon1"] = v1alpha1.AddOn{
+		Version: "1.0.0",
+	}
+	addons["test-addon2"] = v1alpha1.AddOn{
+		Version: "1.0.0",
+		Disable: true,
+	}
+	err := UpdateBases(targetPath, modules, addons)
+	if !assert.NoError(t, err) {
+		return
+	}
+}
+
+func TestUpdateBasesCluster(t *testing.T) {
+	testDirPath := t.TempDir()
+	targetPath := path.Join(testDirPath, "groups/group-1/cluster-1")
+	os.MkdirAll(targetPath, os.ModePerm)
+	err := UpdateBases(targetPath, nil, nil)
+	if !assert.NoError(t, err) {
+		return
+	}
+	file, _ := os.ReadFile(path.Join(targetPath, "bases", utils.KustomizationFileName))
+	t.Log(string(file))
+	compareFile(t, []byte(expectedKustomization), path.Join(targetPath, "bases", utils.KustomizationFileName))
 }
